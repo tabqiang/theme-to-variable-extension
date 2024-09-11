dafbdf418eccc181341048957addc07800ea5ac5
Caps <838269741@qq.com>
2024-09-11 23:08:01 +0800

    feat: collect css properties

--- background.js
+++ background.js
@@ -4,11 +4,14 @@ chrome.runtime.onInstalled.addListener(() => {
 
 //接收来自content.js的消息
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  console.log("Received message from content script");
-  console.log(message);
-  console.log(sender);
-  console.log("Received message from content script");
-  let customProperties = getCustomCss();
-  console.log(customProperties);
-  sendResponse(customProperties);
+  console.log("Message received", message);
+  if (message.type === "GET_CUSTOM_CSS") {
+    console.log("Custom CSS properties", message.customProperties);
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      chrome.tabs.sendMessage(tabs[0].id, {
+        type: "showPopup",
+        customProperties: message.customProperties,
+      });
+    });
+  }
 });
--- content.js
+++ content.js
@@ -1,14 +1,4 @@
 console.log("Hello from content.js");
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.action === "GET_CUSTOM_CSS") {
-    // 在这里处理事件
-    console.log("事件已触发");
-    let customProperties = getCustomCss();
-    console.log(customProperties);
-    sendResponse(customProperties);
-  }
-});
-
 //定义一个函数，收集页面自定义的css属性
 function getCustomCss() {
   let customProperties = {};
@@ -26,3 +16,36 @@ function getCustomCss() {
   }
   return customProperties;
 }
+
+//定义事件类型，调用getCustomCss，发送给background.js
+chrome.runtime.sendMessage({
+  type: "GET_CUSTOM_CSS",
+  customProperties: getCustomCss(),
+});
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.type === "showPopup") {
+    console.log("Show popup", message.customProperties);
+    let popup = document.createElement("div");
+    popup.style.position = "fixed";
+    popup.style.top = "20";
+    popup.style.left = "0";
+    popup.style.width = "80%";
+    popup.style.height = "500px";
+    popup.style.backgroundColor = "white";
+    popup.style.zIndex = "9999";
+    popup.style.display = "flex";
+    popup.style.justifyContent = "center";
+    popup.style.alignItems = "center";
+    popup.style.flexDirection = "column";
+    popup.innerHTML = `
+      <h1>Custom CSS Properties</h1>
+      <ul>
+        ${Object.entries(message.customProperties)
+          .map(([property, value]) => `<li>${property}: ${value}</li>`)
+          .join("")}
+      </ul>
+    `;
+    document.body.appendChild(popup);
+  }
+});
